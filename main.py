354ea0ad3c96acad24496f0e6e1d99eda08ce637
unknown <oussama.cherni@ensi-uma.tn>
2022-05-16 10:59:58 +0100

    Creact API with fastapi and uvicorn

--- main.py
+++ main.py
@@ -6,11 +6,30 @@
 # @Time: 16/05/2022 08:59
 # @Contact :   oussama.cherni@ensi-uma.tn
 # ---------------------------------------------------------------------------
+import json
 
+import uvicorn
+from fastapi import FastAPI, Response
+from scraper import FacebookScraper
 from utils import *
 
 
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    page_name = "TED"
+
+    scraper_fb = FacebookScraper(page_name, 50)
+    scraper_fb.init_driver()
+    scroll_to_bottom(scraper_fb.driver, 8)
+    data = scraper_fb.scrape_data()
+
+    json_data = json.dumps(data, indent=4)
+    return Response(content=json_data, media_type="application/json")
+
+
 if __name__ == "__main__":
-    download_chrome_driver()
-    driver = initialize_driver()
-    driver.quit()
+    uvicorn.run(app='main:app')
+
