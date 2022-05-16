e4255934c2d4c33f865c6948c4f1eebfaf30e03a
unknown <oussama.cherni@ensi-uma.tn>
2022-05-16 10:15:08 +0100

    Create scraper Class

---
+++ scraper.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3  
+# -*- coding: utf-8 -*- 
+# ----------------------------------------------------------------------------
+# @File: scraper
+# @Author: Cherni Oussama
+# @Time: 16/05/2022 09:13
+# @Contact :   oussama.cherni@ensi-uma.tn
+# ---------------------------------------------------------------------------
+
+import json
+from utils import *
+from selenium.webdriver.common.by import By
+
+
+class FacebookScraper:
+    data_dict = {}
+
+    def __init__(self, page_name, timeout):
+        self.page_name = page_name
+        self.URL = get_full_path(page_name)
+        self.driver = None
+        self.timeout = timeout
+
+    def init_driver(self):
+        download_chrome_driver()
+        self.driver = initialize_driver()
+        self.driver.get(self.URL)
+        scroll_down_first(self.driver)
+        close_error_popup(self.driver)
+
+    def scrape_data(self):
+        all_posts = self.driver.find_elements(By.CSS_SELECTOR, '[aria-posinset]')
+        for index, post in enumerate(all_posts):
+            id_ = index
+            text = get_text(post)
+            video = get_link_video(post)
+            shares, comments = get_shares_comments(post)
+            total_reactions = get_reactions(post)
+            posted_time = get_posted_time(post)
+            images = get_images(post)
+
+            self.data_dict[id_] = {
+                "page_name": self.page_name,
+                "shares": shares,
+                "reaction_count": total_reactions,
+                "comments": comments,
+                "content": text,
+                "posted_on": posted_time,
+                "video": video,
+                "image": images,
+            }
+        json_data = json.dumps(self.data_dict, indent=4)
+        return json_data
--- utils.py
+++ utils.py
@@ -8,12 +8,22 @@
 # ---------------------------------------------------------------------------
 
 import os
+import re
+import time
 import zipfile
+from random import randint
+
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from selenium.webdriver import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 def download_chrome_driver():
+
     cwd = os.getcwd()
     folder_driver_path = os.path.join(cwd, "driver")
     driver_path = os.path.join(cwd, "driver", "chromedriver.exe")
@@ -28,6 +38,7 @@ def download_chrome_driver():
     else:
         print("Chrome driver exists ! ")
 
+
 def initialize_driver():
     cwd = os.getcwd()
     driver_path = os.path.join(cwd, "driver", "chromedriver.exe")
@@ -37,3 +48,132 @@ def initialize_driver():
     options.add_argument("--window-size=1920,1200")
     driver = webdriver.Chrome(driver_path, options=options)
     return driver
+
+
+def get_full_path(tag):
+    return "https://facebook.com/{}".format(tag)
+
+
+def get_name(driver):
+    name = driver.find_element(By.TAG_NAME, "strong").get_attribute("textContent")
+    return name
+
+
+def close_error_popup(driver):
+    try:
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[aria-label=Close]")))
+        driver.find_element(By.CSS_SELECTOR, "[aria-label=Close]").click()
+
+    except WebDriverException as e:
+        print(e)
+        pass
+    except NoSuchElementException:
+        pass
+
+    except Exception as ex:
+        print("error at close_error_popup method : {}".format(ex))
+
+
+def scroll_down_first(driver):
+    body = driver.find_element(By.CSS_SELECTOR, "body")
+    for _ in range(randint(5, 6)):
+        body.send_keys(Keys.PAGE_UP)
+    for _ in range(randint(5, 8)):
+        body.send_keys(Keys.PAGE_DOWN)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+
+def check_timeout(start_time, current_time, timeout):
+    return (current_time - start_time) > timeout
+
+
+def scroll_to_bottom(driver, timeout):
+    old_position = 0
+    new_position = None
+    end_position = 0
+    start_time = time.time()
+    body = driver.find_element(By.CSS_SELECTOR, "body")
+
+    while new_position != old_position and not check_timeout(start_time, time.time(), timeout):
+        # Get old scroll position
+        old_position = driver.execute_script(
+            ("return (window.pageYOffset !== undefined) ?"
+             " window.pageYOffset : (document.documentElement ||"
+             " document.body.parentNode || document.body);"))
+        # Sleep and Scroll
+        time.sleep(1)
+        driver.execute_script((
+            "var scrollingElement = (document.scrollingElement ||"
+            " document.body);scrollingElement.scrollTop ="
+            " scrollingElement.scrollHeight;"))
+        # Get new position
+        new_position = driver.execute_script(
+            ("return (window.pageYOffset !== undefined) ?"
+             " window.pageYOffset : (document.documentElement ||"
+             " document.body.parentNode || document.body);"))
+
+        if end_position != new_position:
+            for _ in range(randint(5, 6)):
+                body.send_keys(Keys.PAGE_UP)
+        end_position = new_position
+    if new_position == old_position:
+        print("All")
+    else:
+        print("Timeout")
+
+
+def get_text(post):
+    try:
+        div_text = post.find_element(By.CSS_SELECTOR, '[data-ad-comet-preview]')
+        text = div_text.get_attribute('innerText')
+    except:
+        text = None
+    return text
+
+
+def get_link_video(post):
+    try:
+        div_text = post.find_element(By.CSS_SELECTOR, "[aria-label='Enlarge']")
+        text = div_text.get_attribute('href')
+        text = text[:text.rfind("/")]
+    except:
+        text = None
+    return text
+
+
+def get_shares_comments(post):
+    elements = post.find_elements(By.CSS_SELECTOR, "div.gtad4xkn")
+    shares = "0"
+    comments = "0"
+    for element in elements:
+        text = element.get_attribute("innerText")
+        if "Shares" in text:
+            shares = re.findall("\d+", text)[0]
+
+        if "Comments" in text:
+            comments = re.findall("\d+", text)[0]
+
+    return shares, comments
+
+
+def get_reactions(post):
+    reactions = 0
+    try:
+        total_reactions = post.find_element(By.CSS_SELECTOR, 'span.ltmttdrg.gjzvkazv')
+        reactions = total_reactions.get_attribute("innerText")
+    except Exception as e:
+        print(e)
+    return reactions
+
+
+def get_posted_time(post):
+    element = post.find_element(By.CSS_SELECTOR, "a.gpro0wi8.b1v8xokw")
+    print(element.get_attribute('innerText'))
+
+
+def get_images(post):
+    list_of_images = []
+    elements = post.find_elements(By.CSS_SELECTOR, 'div.do00u71z.ni8dbmo4.stjgntxs.l9j0dhe7 img.i09qtzwb')
+    for element in elements:
+        list_of_images.append(element.get_attribute('src'))
+    return list_of_images
